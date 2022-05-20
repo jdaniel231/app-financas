db00a03e5db5dd1b2db128e62af59f8209daa28c
hammer200911 <jdaniel231@gmail.com>
2022-05-20 12:26:03 -0300

    criar movimentação

---
+++ app/models/movimentacao.rb
@@ -0,0 +1,22 @@
+class Movimentacao < ApplicationRecord
+  enum :tipo, { saida: 'saida', entrada: 'entrada' }
+
+  validates :data, comparison: { less_than_or_equal_to: proc { Date.current } }
+  validates :descricao, presence: true
+  validates :descricao, length: { maximum: 150 }
+  validates :valor, presence: true
+  validates :tipo, presence: true
+
+  validate :valida_se_existe_saldo
+
+  def self.saldo_atual
+    self.entrada.sum(:valor) - self.saida.sum(:valor)
+  end
+
+  def valida_se_existe_saldo
+    return if entrada?
+    return if valor.to_f <= Movimentacao.saldo_atual
+    
+    errors.add :valor, 'Não há saldo suficiente'
+  end
+end
